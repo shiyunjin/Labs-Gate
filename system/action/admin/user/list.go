8d0327c7a9ac74a365d1ed5ce9015145b98fe3fd
shiyunjin <admin@shiyunjin.cn>
2018-10-25 23:51:55 +0800

    feat(user): add user list

---
+++ system/action/admin/user/list.go
@@ -0,0 +1,56 @@
+package user
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shiyunjin/SchoolNetwork/system/e"
+	"github.com/shiyunjin/SchoolNetwork/system/model"
+	"gopkg.in/mgo.v2"
+)
+
+
+type ListResponse struct {
+	Username	string	`json:"username"`
+	Name		string	`json:"name"`
+	Auth 		string	`json:"auth"`
+	Date 		string	`json:"date"`
+}
+
+
+func List(c *gin.Context) {
+	//session := sessions.Default(c)
+	//user := session.Get("NowUser").(*util.Claims)
+
+	db := c.MustGet("db").(*mgo.Database)
+	users := []model.User{}
+	err := db.C(model.CollectionUser).Find(nil).Sort("createtime").All(&users)
+	if err != nil {
+		c.Error(err)
+	}
+
+	var all, user, admin []ListResponse
+
+	for _, row := range users {
+		tempUser := ListResponse{
+			Username:	row.Username,
+			Name:		row.Name,
+			Auth: 		row.Permission,
+			Date: 		row.Createtime.Format("2006-01-02 15:04:05"),
+		}
+		all = append(all, tempUser)
+		if row.Permission == "admin" {
+			user = append(user, tempUser)
+		} else {
+			admin = append(admin, tempUser)
+		}
+	}
+	c.JSON(e.SUCCESS, gin.H{
+		"status" : e.SUCCESS,
+		"statusText" : e.GetMsg(e.SUCCESS),
+		"data" : gin.H{
+			"all": all,
+			"user": user,
+			"admin": admin,
+		},
+	})
+
+}
