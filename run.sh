bb3180815231cc5015a4f91a3ca60b0487780421
shiyunjin <admin@shiyunjin.cn>
2018-11-10 00:23:25 +0800

    fix(docker): fix run.sh can't replace config file

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-if [ ! -f "config.tson" ]; then
-    sed -i 's/MONGODB_HOST/'"$MONGODB_HOST"'/g' config.tson
-    sed -i 's/MONGODB_PORT/'"$MONGODB_PORT"'/g' config.tson
-    sed -i 's/MONGODB_NAME/'"$MONGODB_NAME"'/g' config.tson
-    sed -i 's/MONGODB_USER/'"$MONGODB_USER"'/g' config.tson
-    sed -i 's/MONGODB_PASS/'"$MONGODB_PASS"'/g' config.tson
-
-    sed -i 's/JWT_SECRET/'"$JWT_SECRET"'/g' config.tson
-    sed -i 's/MAIN_SECRET/'"$MAIN_SECRET"'/g' config.tson
-
-    mv config.tson config.json
+if [ ! -f "config.json" ]; then
+    sed -e "s/MONGODB_HOST/$MONGODB_HOST/" \
+        -e "s/MONGODB_PORT/$MONGODB_PORT/" \
+        -e "s/MONGODB_NAME/$MONGODB_NAME/" \
+        -e "s/MONGODB_USER/$MONGODB_USER/" \
+        -e "s/MONGODB_PASS/$MONGODB_PASS/" \
+        -e "s/JWT_SECRET/$JWT_SECRET/" \
+        -e "s/MAIN_SECRET/$MAIN_SECRET/" config.tson > config.json
 fi
 
 exec "/app/lab-gate"
