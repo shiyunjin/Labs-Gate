c3a1ed0b7c146a81395ac905d545bf4a5067cdd4
shiyunjin <admin@shiyunjin.cn>
2018-11-15 20:40:15 +0800

    test(rom): add rom func test

---
+++ system/action/rom/rom_test.go
@@ -0,0 +1,46 @@
+package rom
+
+import (
+	"github.com/shiyunjin/Labs-Gate/system/e"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestList(t *testing.T) {
+	server := testGinWithLogin()
+
+	server.GET("/roms", List)
+
+
+	req := httptest.NewRequest(http.MethodGet, "/roms", nil)
+
+	w := httptest.NewRecorder()
+
+	server.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != e.SUCCESS {
+		t.Fatalf("get rom list test error")
+	}
+}
+
+func TestMachine(t *testing.T) {
+	server := testGinWithLogin()
+
+	server.GET("/machine", List)
+
+
+	req := httptest.NewRequest(http.MethodGet, "/machine", nil)
+
+	w := httptest.NewRecorder()
+
+	server.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != e.SUCCESS {
+		t.Fatalf("get rom list test error")
+	}
+}
