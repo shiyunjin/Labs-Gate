58cee31fb37d99bdcf1215e931e184c6b7fd9fd9
shiyunjin <admin@shiyunjin.cn>
2018-11-16 12:55:17 +0800

    test(db): add TestConnectPanic func

--- system/db/db_test.go
+++ system/db/db_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/shiyunjin/Labs-Gate/system/config"
 	"testing"
@@ -12,4 +13,21 @@ func TestConnect(t *testing.T) {
 	config.Init()
 
 	Connect()
+}
+
+func TestConnectPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("mongodb connect panic success", r)
+		}
+	}()
+	gin.SetMode(gin.TestMode)
+
+	config.Init()
+
+	config.Set("mongodb.username", "failuser")
+	config.Set("mongodb.password", "failuser")
+
+	Connect()
+	t.Errorf("mongodb did not panic")
 }
